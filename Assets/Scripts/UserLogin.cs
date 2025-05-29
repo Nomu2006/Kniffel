ca2880b6adcf8cd69697238e71cd5a24f20aedd5
ElPol4k0 <pawel.wiercioch93@gmail.com>
2025-05-29 11:46:53 +0200

    fix: Change Scene name

--- Assets/Scripts/UserLogin.cs
+++ Assets/Scripts/UserLogin.cs
@@ -1,16 +1,69 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class UserLogin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Input Fields")] public TMP_InputField usernameLoginInput;
+    public TMP_InputField passwordLoginInput;
+
+    [Header("Error and Success Display")] public TMP_Text loginErrorText;
+    public TMP_Text loginSuccessText;
+
+    private string filePath;
+
+    private void Start()
     {
-        
+        filePath = Application.dataPath + "/Database/users.json";
+        Debug.Log("User file path: " + filePath);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoginUser()
     {
-        
+        string username = usernameLoginInput.text.Trim();
+        string password = passwordLoginInput.text;
+
+        if (ExistUser(username, password))
+        {
+            loginSuccessText.text = "Login Successful!";
+            loginSuccessText.gameObject.SetActive(true);
+            loginErrorText.text = "";
+            SceneManager.LoadScene("MainMenuScene");
+        }
+        else
+        {
+            loginErrorText.text = "Username or password is incorrect.";
+            loginSuccessText.gameObject.SetActive(false);
+        }
+    }
+
+    public bool ExistUser(string username, string password)
+    {
+        // Datei einlesen
+        string json = File.ReadAllText(filePath);
+        UserList userList = JsonUtility.FromJson<UserList>(json);
+
+        foreach (UserData user in userList.users)
+        {
+            if (user.userName.Equals(username, StringComparison.OrdinalIgnoreCase))
+            {
+                if (user.password == password)
+                {
+                    return true;
+                }
+                else
+                {
+                    Debug.Log(user.userName + " doesn't match");
+                    return false; // Benutzername korrekt, Passwort falsch
+                }
+            }
+        }
+
+        Debug.Log("Username not found: " + username);
+        return false; // Benutzername nicht gefunden
     }
 }
